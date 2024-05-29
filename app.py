522f8e171280c9913e4a6c112278547857939e9b
Q.A.zh <40236765+QAbot-zh@users.noreply.github.com>
2024-05-29 13:57:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def submit():
         return render_template('index.html', response=show_info)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=os.getenv("PORT", default=5000), debug=True)
